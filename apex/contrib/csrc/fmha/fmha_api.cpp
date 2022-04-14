91a358cd34bf9bc71265d64eb602b4853b00c669
sneaxiy <sneaxiy@126.com>
2022-04-14 08:29:49 +0000

    update fmhalib API

--- apex/contrib/csrc/fmha/fmha_api.cpp
+++ apex/contrib/csrc/fmha/fmha_api.cpp
@@ -38,6 +38,8 @@
 #include "fmhalib.h"
 #include "dlfcn.h"
 
+using SeedIncFuncPtr = void (*)(uint64_t, uint64_t *, const int64_t **, uint64_t*, bool*);
+
 #define ASSERT_CHECK(__cond)                             \
       do {                                               \
         const bool __cond_var = (__cond);                \
@@ -74,76 +76,6 @@ const char *fmhalib_error() {
 
 #define FMHALIB_BEGIN_FUNC try {
 #define FMHALIB_END_FUNC } catch (::std::exception &__e) { fmhalib_set_error(__e.what()); } catch (...) { fmhalib_set_error(nullptr); }
-
-struct FMHALibSeedManager {
- private:
-  FMHALibSeedManager() {
-    int64_t *tmp;
-    ASSERT_CHECK(cudaMalloc(&tmp, sizeof(*tmp)) == cudaSuccess);
-    gpu_offset_ptr = tmp;
-    seed = 67280421310721;
-    cpu_offset = 0;
-    gpu_offset = 0;
-    is_device_rnd = false;
-  }
- 
- public: 
-  static FMHALibSeedManager &Instance() {
-    static FMHALibSeedManager instance;
-    return instance;
-  }
-
-  ~FMHALibSeedManager() {
-    if (gpu_offset_ptr) cudaFree(gpu_offset_ptr);
-  }
-
-  uint64_t seed;
-  uint64_t cpu_offset;
-  int64_t *gpu_offset_ptr;
-  uint32_t gpu_offset;
-  bool is_device_rnd;
-  std::mutex mtx;
-};	
-
-void fmhalib_random_seed(const uint64_t rnd_seed) {
-  FMHALIB_BEGIN_FUNC
-  auto &g_seed_manager = FMHALibSeedManager::Instance();  
-  std::lock_guard<std::mutex> guard(g_seed_manager.mtx); 
-  g_seed_manager.seed = rnd_seed;
-  g_seed_manager.cpu_offset = 0;
-  g_seed_manager.gpu_offset = 0;
-  FMHALIB_END_FUNC
-}
-
-void fmhalib_random_state(const uint64_t increment,
-		          uint64_t *rnd_seed,
-		          int64_t **offset_ptr,
-		          uint64_t *rnd_offset,
-		          bool *is_device_rnd) {
-  FMHALIB_BEGIN_FUNC
-  ASSERT_CHECK(rnd_seed != nullptr);
-  ASSERT_CHECK(offset_ptr != nullptr);
-  ASSERT_CHECK(rnd_offset != nullptr);
-  ASSERT_CHECK(is_device_rnd != nullptr);
-
-  auto inc = ((increment + 3) / 4) * 4;
-
-  auto &g_seed_manager = FMHALibSeedManager::Instance();
-  std::lock_guard<std::mutex> guard(g_seed_manager.mtx);
-  *rnd_seed = g_seed_manager.seed;
-  if (g_seed_manager.is_device_rnd) {
-    *rnd_offset = static_cast<uint64_t>(g_seed_manager.gpu_offset);
-    *offset_ptr = g_seed_manager.gpu_offset_ptr;
-    *is_device_rnd = true;
-    g_seed_manager.gpu_offset += static_cast<uint32_t>(inc);
-  } else {
-    *rnd_offset = g_seed_manager.cpu_offset; 
-    *offset_ptr = nullptr;
-    *is_device_rnd = false;
-    g_seed_manager.cpu_offset += inc;  
-  }
-  FMHALIB_END_FUNC
-}
 
 static void set_params(Fused_multihead_attention_fprop_params &params,
                        // sizes
@@ -198,7 +130,7 @@ static void set_params(Fused_multihead_attention_fprop_params &params,
     set_alpha(params.scale_dropout, params.rp_dropout, data_type);
 }
 
-int fmhalib_random_increment(const int seq_len) {
+static int fmhalib_random_increment(const int seq_len) {
     constexpr int warps_m = 1;
     constexpr int warps_n = 4;  // this leads to an upper bound
     const int mmas_m = seq_len / 16 / warps_m;
@@ -208,7 +140,7 @@ int fmhalib_random_increment(const int seq_len) {
     return elts_per_thread;
 }
 
-int fmhalib_bwd_nl_num_chunks(const int batch_size) {
+static int fmhalib_bwd_nl_num_chunks(const int batch_size) {
     int num_chunks = 2;
     if( batch_size == 1 ) {
         num_chunks = 4;
@@ -234,6 +166,68 @@ int fmhalib_seq_len(const int max_seq_len) {
     return -1;
 }
 
+static void SetPhiloxCudaState(at::PhiloxCudaState *state, SeedIncFuncPtr seed_inc_func, uint64_t increment) {
+    uint64_t rnd_seed;
+    const int64_t *offset_ptr;   
+    uint64_t rnd_offset;
+    bool is_device_rnd;
+    seed_inc_func(increment, &rnd_seed, &offset_ptr, &rnd_offset, &is_device_rnd);
+    if (is_device_rnd) {
+        *state = at::PhiloxCudaState(rnd_seed, const_cast<int64_t *>(offset_ptr), static_cast<uint32_t>(rnd_offset));  
+    } else {
+        *state = at::PhiloxCudaState(rnd_seed, rnd_offset); 
+    } 
+}
+
+static void fmhalib_fwd_nl(const void *qkv_ptr,
+                           const void *cu_seqlens_ptr,
+                           const int total,
+                           const int num_heads,
+                           const int head_size,
+                           const int batch_size,
+                           const float p_dropout,
+                           const int max_seq_len,
+                           const bool is_training,
+                           const bool zero_tensors,
+                           SeedIncFuncPtr seed_inc_func,
+                           cudaStream_t stream,
+                           void *ctx_ptr,
+                           void *s_ptr) {
+    FMHALIB_BEGIN_FUNC
+    int seq_len = 512;
+    auto launch = &run_fmha_fp16_512_64_sm80_nl;
+    ASSERT_CHECK(max_seq_len == seq_len);
+    ASSERT_CHECK(batch_size > 0);
+    ASSERT_CHECK(head_size == 64);
+
+    Fused_multihead_attention_fprop_params params;
+
+    set_params(params,
+               batch_size,
+               seq_len,
+               num_heads,
+               head_size,
+               const_cast<void*>(qkv_ptr),
+               const_cast<void*>(cu_seqlens_ptr),
+               ctx_ptr,
+               s_ptr,
+               p_dropout);
+
+    if( is_training ) {
+        auto increment = fmhalib_random_increment(seq_len);
+        SetPhiloxCudaState(&params.philox_args, seed_inc_func, increment);
+    }
+
+    int num_chunks = 3;
+    if(batch_size == 3) {
+        num_chunks = 2;
+    }
+
+    launch(params, is_training, num_chunks, stream);
+    FMHALIB_END_FUNC
+}
+
+
 void fmhalib_fwd(const void *qkv_ptr, 
 	         const void *cu_seqlens_ptr,
 		 const int total,
@@ -243,13 +237,19 @@ void fmhalib_fwd(const void *qkv_ptr,
 	         const float p_dropout, 
 	         const int max_seq_len,
 	         const bool is_training, 
-	         const uint64_t rnd_seed, 
-		 const int64_t *offset_ptr,
-	         const uint64_t rnd_offset,
-		 const bool is_device_rnd,
+                 const bool is_nl,
+                 const bool zero_tensors,
+                 SeedIncFuncPtr seed_inc_func,
 	         cudaStream_t stream,
 	         void *ctx_ptr,
 	         void *s_ptr) {
+    if (is_nl) {
+      fmhalib_fwd_nl(qkv_ptr, cu_seqlens_ptr, total, num_heads,
+          head_size, batch_size, p_dropout, max_seq_len, is_training,
+          zero_tensors, seed_inc_func, stream, ctx_ptr, s_ptr); 
+      return;
+    }
+
     FMHALIB_BEGIN_FUNC
     int seq_len;
     auto launch = &run_fmha_fp16_512_64_sm80;
@@ -285,154 +285,28 @@ void fmhalib_fwd(const void *qkv_ptr,
                p_dropout);
 
     if (is_training) {
-	if (is_device_rnd) {
-	    params.philox_args = at::PhiloxCudaState(rnd_seed, const_cast<int64_t *>(offset_ptr), static_cast<uint32_t>(rnd_offset));
-        } else {
-	    params.philox_args = at::PhiloxCudaState(rnd_seed, rnd_offset);
-	}
+        auto increment = fmhalib_random_increment(seq_len);
+        SetPhiloxCudaState(&params.philox_args, seed_inc_func, increment);
     }
 
     launch(params, is_training, stream);
     FMHALIB_END_FUNC
 }
 
-void fmhalib_bwd(const void *dout_ptr,
-	         const void *qkv_ptr,
-	         const void *cu_seqlens_ptr,
-	         const int total,
-                 const int num_heads,
-                 const int head_size,
-	         const int batch_size,
-                 const float p_dropout,         // probability to drop
-                 const int max_seq_len,         // max sequence length to choose the kernel
-		 cudaStream_t stream,
-		 void *softmax_ptr,             // will be overwritten 
-		 void *dqkv_ptr 
-) {
-    FMHALIB_BEGIN_FUNC
-    int seq_len = 512;
-    auto launch = &run_fmha_dgrad_fp16_512_64_sm80;
-    if( max_seq_len <= 128 ) {
-        seq_len = 128;
-        launch = &run_fmha_dgrad_fp16_128_64_sm80;
-    } else if( max_seq_len <= 256 ) {
-        seq_len = 256;
-        launch = &run_fmha_dgrad_fp16_256_64_sm80;
-    } else if( max_seq_len <= 384 ) {
-        seq_len = 384;
-        launch = &run_fmha_dgrad_fp16_384_64_sm80;
-    } else if( max_seq_len <= 512 ) {
-        seq_len = 512;
-        launch = &run_fmha_dgrad_fp16_512_64_sm80;
-    } else {
-	ASSERT_CHECK(false);
-    }
-
-    ASSERT_CHECK(batch_size > 0);
-    ASSERT_CHECK(head_size == 64);
-
-    Fused_multihead_attention_fprop_params params;
-
-    set_params(params,
-               batch_size,
-               seq_len,
-               num_heads,
-               head_size,
-               const_cast<void*>(qkv_ptr),
-               const_cast<void*>(cu_seqlens_ptr),
-               const_cast<void*>(dout_ptr), // we set o_ptr to dout
-               softmax_ptr,  // softmax gets overwritten by dP!
-               p_dropout);
-
-    // we're re-using these scales
-    Data_type acc_type = DATA_TYPE_FP32;
-    set_alpha(params.scale_bmm1, 1.f, acc_type);
-    set_alpha(params.scale_softmax, 1.f / sqrtf(head_size), acc_type);
-    set_alpha(params.scale_bmm2, 1.f, DATA_TYPE_FP16);
-    params.dqkv_ptr = dqkv_ptr;
-
-    launch(params, stream);
-    FMHALIB_END_FUNC
-}
-
-void fmhalib_fwd_nl(const void *qkv_ptr,
-                    const void *cu_seqlens_ptr,
-                    const int total,
-                    const int num_heads,
-                    const int head_size,
-                    const int batch_size,
-                    const float p_dropout,
-                    const int max_seq_len,
-                    const bool is_training,
-                    const uint64_t rnd_seed,
-                    const int64_t *offset_ptr,
-                    const uint64_t rnd_offset,
-                    const bool is_device_rnd,
-                    cudaStream_t stream,
-                    void *ctx_ptr,
-                    void *s_ptr) {
-    FMHALIB_BEGIN_FUNC
-    int seq_len = 512;
-    auto launch = &run_fmha_fp16_512_64_sm80_nl;
-    ASSERT_CHECK(max_seq_len == seq_len);
-
-    constexpr int warps_m = 1;
-    constexpr int warps_n = 4;  // this leads to an upper bound
-    const int mmas_m = seq_len / 16 / warps_m;
-    const int mmas_n = seq_len / 16 / warps_n;
-    // static_assert( mmas_m == 32 );
-    // static_assert( mmas_n == 4 );
-    const int elts_per_thread = 8 * mmas_m * mmas_n;
-
-    ASSERT_CHECK(batch_size > 0);
-    ASSERT_CHECK(head_size == 64);
-
-    Fused_multihead_attention_fprop_params params;
-
-    set_params(params,
-               batch_size,
-               seq_len,
-               num_heads,
-               head_size,
-               const_cast<void*>(qkv_ptr),
-               const_cast<void*>(cu_seqlens_ptr),
-               ctx_ptr,
-               s_ptr,
-               p_dropout);
-
-    // number of times random will be generated per thread, to offset philox counter in the random
-    // state
-
-    if( is_training ) {
-        if (is_device_rnd) {
-            params.philox_args = at::PhiloxCudaState(rnd_seed, const_cast<int64_t *>(offset_ptr), static_cast<uint32_t>(rnd_offset));
-        } else {
-            params.philox_args = at::PhiloxCudaState(rnd_seed, rnd_offset);
-        }
-    }
-    int num_chunks = 3;
-    if(batch_size == 3) {
-        num_chunks = 2;
-    }
-
-    launch(params, is_training, num_chunks, stream);
-    FMHALIB_END_FUNC
-}
-
-void fmhalib_bwd_nl(const void *dout_ptr,
-                    const void *qkv_ptr,
-                    const void *cu_seqlens_ptr,
-                    const int total,
-                    const int num_heads,
-                    const int head_size,
-                    const int batch_size,
-                    const float p_dropout,         // probability to drop
-                    const int max_seq_len,         // max sequence length to choose the kernel
-                    cudaStream_t stream,
-                    void *softmax_ptr,             // will be overwritten
-                    void *dqkv_ptr,
-		    void *dkv_ptr
-) {
+static void fmhalib_bwd_nl(const void *dout_ptr,
+                           const void *qkv_ptr,
+                           const void *cu_seqlens_ptr,
+                           const int total,
+                           const int num_heads,
+                           const int head_size,
+                           const int batch_size,
+                           const float p_dropout,         // probability to drop
+                           const int max_seq_len,         // max sequence length to choose the kernel
+                           const bool zero_tensors,
+                           cudaStream_t stream,
+                           void *softmax_ptr,             // will be overwritten
+                           void *dqkv_ptr,
+                           void *dkv_ptr) {
     FMHALIB_BEGIN_FUNC
 
     ASSERT_CHECK(batch_size > 0);
@@ -481,45 +355,86 @@ void fmhalib_bwd_nl(const void *dout_ptr,
     FMHALIB_END_FUNC
 }
 
+
+void fmhalib_bwd(const void *dout_ptr,
+                 const void *qkv_ptr,
+                 const void *cu_seqlens_ptr,
+                 const int total,
+                 const int num_heads,
+                 const int head_size,
+                 const int batch_size,
+                 const float p_dropout,
+                 const int max_seq_len,
+                 const bool is_nl,
+                 const bool zero_tensors,
+                 cudaStream_t stream,
+                 void *softmax_ptr,  // will be overwritten
+                 void *dqkv_ptr,
+                 void *workspace,
+                 uint64_t *workspace_size) {
+    if (dqkv_ptr == nullptr) {
+        if (is_nl) {
+            int num_chunks = fmhalib_bwd_nl_num_chunks(batch_size); 
+            *workspace_size = static_cast<uint64_t>(total) * num_chunks * 2 * num_heads * head_size * 2;  
+        } else {
+            *workspace_size = 0;
+        }
+        return;
+    }
+
+    if (is_nl) {
+        fmhalib_bwd_nl(dout_ptr, qkv_ptr, cu_seqlens_ptr, total, num_heads, head_size,
+            batch_size, p_dropout, max_seq_len, zero_tensors, stream, softmax_ptr,
+            dqkv_ptr, workspace);
+        return;
+    }
+
+    FMHALIB_BEGIN_FUNC
+    int seq_len = 512;
+    auto launch = &run_fmha_dgrad_fp16_512_64_sm80;
+    if( max_seq_len <= 128 ) {
+        seq_len = 128;
+        launch = &run_fmha_dgrad_fp16_128_64_sm80;
+    } else if( max_seq_len <= 256 ) {
+        seq_len = 256;
+        launch = &run_fmha_dgrad_fp16_256_64_sm80;
+    } else if( max_seq_len <= 384 ) {
+        seq_len = 384;
+        launch = &run_fmha_dgrad_fp16_384_64_sm80;
+    } else if( max_seq_len <= 512 ) {
+        seq_len = 512;
+        launch = &run_fmha_dgrad_fp16_512_64_sm80;
+    } else {
+	ASSERT_CHECK(false);
+    }
+
+    ASSERT_CHECK(batch_size > 0);
+    ASSERT_CHECK(head_size == 64);
+
+    Fused_multihead_attention_fprop_params params;
+
+    set_params(params,
+               batch_size,
+               seq_len,
+               num_heads,
+               head_size,
+               const_cast<void*>(qkv_ptr),
+               const_cast<void*>(cu_seqlens_ptr),
+               const_cast<void*>(dout_ptr), // we set o_ptr to dout
+               softmax_ptr,  // softmax gets overwritten by dP!
+               p_dropout);
+
+    // we're re-using these scales
+    Data_type acc_type = DATA_TYPE_FP32;
+    set_alpha(params.scale_bmm1, 1.f, acc_type);
+    set_alpha(params.scale_softmax, 1.f / sqrtf(head_size), acc_type);
+    set_alpha(params.scale_bmm2, 1.f, DATA_TYPE_FP16);
+    params.dqkv_ptr = dqkv_ptr;
+
+    launch(params, stream);
+    FMHALIB_END_FUNC
+}
+
 #ifdef __cplusplus
 }
-#endif
-
-#ifdef __cplusplus
-namespace fmhalib {
-
-static void *OpenFMHALibHandle() {
-  const char *env_var = "FMHALIB_PATH";
-  const char *lib_path = std::getenv(env_var);
-  if (!lib_path) {
-    lib_path = "libfmha.so";
-  }
-  void *handle = dlopen(lib_path, RTLD_LAZY);
-  if (!handle) {
-    throw std::runtime_error(dlerror());
-  }
-  return handle;
-}
-
-static void *GetFMHALibSymbol(const char *name) {
-  static void *lib_handle = OpenFMHALibHandle();
-  void *symbol = dlsym(lib_handle, name);
-  if (!symbol) {
-    throw std::runtime_error(dlerror());
-  }
-  return symbol;
-}
-
-#define _DYLOAD_CXX_FMHALIB_FUNC(__func_name)               \
-   auto DynLoad_##fmhalib_##__func_name()                   \
-           -> decltype(&::fmhalib_##__func_name) {          \
-     using __FuncType = decltype(&::fmhalib_##__func_name); \
-     static auto *__func = reinterpret_cast<__FuncType>(    \
-         GetFMHALibSymbol("fmhalib_"#__func_name));         \
-     return __func;                                         \
-   }
-
-_CXX_FMHALIB_FOR_EACH_FUNC(_DYLOAD_CXX_FMHALIB_FUNC);
-
-} // namespace fmhalib
 #endif
--- build_scripts/fmhalib.h
+++ build_scripts/fmhalib.h
@@ -31,6 +31,7 @@ const char *fmhalib_error();
  */
 int fmhalib_seq_len(const int seq_len);
 
+
 /**
  * qkv_ptr: FP16 Tensor with shape [total, 3, num_heads, head_size] 
  * cu_seqlens_ptr: INT32 Tensor with shape [batch_size + 1]
@@ -41,10 +42,9 @@ int fmhalib_seq_len(const int seq_len);
  * p_dropout: dropout probability
  * max_seq_len: must be <= 512
  * is_training: whether to run train or inference
- * rnd_seed: the random seed
- * offset_ptr: the device pointer to generate the random seed. It can be NULL if is_device_rnd == false. 
- * rnd_offset: the random seed offset.
- * is_device_rnd: whether to generate the random seed in the device side. 
+ * is_nl: whether to run the nl kernel  
+ * zero_tensors: whether to zero output tensors before applying forward  
+ * seed_inc_func: the seed increment function pointer.   
  * stream: the CUDA stream.
  *
  * ctx_ptr: output FP16 tensor with shape [total, num_heads, head_size]
@@ -59,33 +59,12 @@ void fmhalib_fwd(const void *qkv_ptr,
                  const float p_dropout,
                  const int max_seq_len,
                  const bool is_training,
-                 const uint64_t rnd_seed,
-                 const int64_t *offset_ptr,
-                 const uint64_t rnd_offset,
-                 const bool is_device_rnd,
+                 const bool is_nl,
+                 const bool zero_tensors,
+                 void (*seed_inc_func)(uint64_t, uint64_t *, const int64_t **, uint64_t*, bool *),
                  cudaStream_t stream,
                  void *ctx_ptr,
                  void *s_ptr);
-
-/**
- * Almost the same with fmhalib_fwd except for that max_seq_len must be 512.
- */
-void fmhalib_fwd_nl(const void *qkv_ptr,
-                    const void *cu_seqlens_ptr,
-                    const int total,
-                    const int num_heads,
-                    const int head_size,
-                    const int batch_size,
-                    const float p_dropout,
-                    const int max_seq_len,
-                    const bool is_training,
-                    const uint64_t rnd_seed,
-                    const int64_t *offset_ptr,
-                    const uint64_t rnd_offset,
-                    const bool is_device_rnd,
-                    cudaStream_t stream,
-                    void *ctx_ptr,
-                    void *s_ptr);
 
 /**
  * dout_ptr: the gradient of the output `ctx_ptr` in fmhalib_fwd
@@ -97,10 +76,15 @@ void fmhalib_fwd_nl(const void *qkv_ptr,
  * batch_size: same with the fmhalib_fwd
  * p_dropout: same with the fmhalib_fwd
  * max_seq_len: same with the fmhalib_fwd
+ * is_nl: same with the fmhalib_fwd 
+ * zero_tensors: same with the fmhalib_fwd 
  * stream: the CUDA stream
  *
  * softmax_ptr: the output `s_ptr` in fmhalib_fwd. Note that it may be overwritten inside the function!   
  * dqkv_ptr: the gradient of the input `qkv_ptr` in fmhalib_fwd
+ * workspace: the workspace pointer
+ * workspace_size: the workspace size. If dqkv_ptr == nullptr, no computation would be performed, and
+ *                 the workspace size would be returned.
  */
 void fmhalib_bwd(const void *dout_ptr,
                  const void *qkv_ptr,
@@ -111,94 +95,13 @@ void fmhalib_bwd(const void *dout_ptr,
                  const int batch_size,
                  const float p_dropout, 
                  const int max_seq_len,
+                 const bool is_nl,
+                 const bool zero_tensors,
                  cudaStream_t stream,
                  void *softmax_ptr,  // will be overwritten
-                 void *dqkv_ptr);
-
-/**
- * Almost the same with fmhalib_bwd except for that max_seq_len must be 512.
- * There is an extra FP16 output dkv_ptr with shape [total, num_chunks, 2, num_heads, head_size],
- * where num_chunks can be obtained by calling fmhalib_bwd_nl_num_chunks(batch_size). 
- */
-void fmhalib_bwd_nl(const void *dout_ptr,
-                    const void *qkv_ptr,
-                    const void *cu_seqlens_ptr,
-                    const int total,
-                    const int num_heads,
-                    const int head_size,
-                    const int batch_size,
-                    const float p_dropout,
-                    const int max_seq_len,
-                    cudaStream_t stream,
-                    void *softmax_ptr,  // will be overwritten
-                    void *dqkv_ptr,
-		    void *dkv_ptr);
-
-/**
- * Set the random seed to the inner seed manager. 
- *
- * This function may call `cudaMalloc` when it is
- * called at the first time.
- *
- * This function may report error when `cudaMalloc`
- * fails. Please get the error by calling
- * `fmhalib_error()`.   
- */
-void fmhalib_random_seed(const uint64_t rnd_seed); 
-
-/**
- * Increase the state of the inner seed manager by
- * `increment`. The `increment` can be obtained by
- * calling `fmhalib_random_increment(seq_len)`.  
- *
- * This function may report error when any of
- * `rnd_seed`, `offset_ptr`, `rnd_offset` and
- * `is_device_rnd` is NULL. Please get the error by 
- * calling `fmhalib_error()`.
- *
- * TODO(zengjinle): support is_device_rnd = true.
- */
-void fmhalib_random_state(const uint64_t increment,
-                          uint64_t *rnd_seed,
-                          int64_t **offset_ptr,
-                          uint64_t *rnd_offset,
-                          bool *is_device_rnd);
-
-// This function never reports error 
-int fmhalib_random_increment(const int seq_len);
-
-// This function never reports error 
-int fmhalib_bwd_nl_num_chunks(const int batch_size);
-
+                 void *dqkv_ptr,
+                 void *workspace,
+                 uint64_t *workspace_size);
 #ifdef __cplusplus
 }
-#endif
-
-#ifdef __cplusplus
-namespace fmhalib {
-
-#define _DEFINE_CXX_FMHALIB_DYNLOAD_FUNC(__func_name)     \
-   extern decltype(&fmhalib_##__func_name)                \
-       DynLoad_##fmhalib_##__func_name();                 \
-   template <typename ...__ARGS>                          \
-   inline auto __func_name(__ARGS... __args)              \
-       -> decltype(::fmhalib_##__func_name(__args...)) {  \
-     return DynLoad_##fmhalib_##__func_name()(__args...); \
-   }
-
-#define _CXX_FMHALIB_FOR_EACH_FUNC(__macro) \
-   __macro(error);                          \
-   __macro(seq_len);                        \
-   __macro(fwd);                            \
-   __macro(bwd);                            \
-   __macro(fwd_nl);                         \
-   __macro(bwd_nl);                         \
-   __macro(random_seed);                    \
-   __macro(random_state);                   \
-   __macro(random_increment);               \
-   __macro(bwd_nl_num_chunks)
-
-_CXX_FMHALIB_FOR_EACH_FUNC(_DEFINE_CXX_FMHALIB_DYNLOAD_FUNC);
-
-} // namespace fmhalib
 #endif
