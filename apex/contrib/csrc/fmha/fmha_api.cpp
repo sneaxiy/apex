54db046b644bd10c13075ff17d011b644293a86b
limin29 <limin29@baidu.com>
2022-04-22 13:04:33 +0000

    support group in batch.

--- apex/contrib/csrc/fmha/fmha_api.cpp
+++ apex/contrib/csrc/fmha/fmha_api.cpp
@@ -191,8 +191,21 @@ static void SetZero(void *ptr, size_t sizeof_type, std::initializer_list<int> sh
     ASSERT_CHECK(cudaMemsetAsync(ptr, 0, n, stream) == cudaSuccess); 
 }
 
+int seq_len_round(int real_seq_len) {
+   if (real_seq_len > 384) {
+     return 512;
+   } else if (real_seq_len > 128) {
+     return 384; 
+   } else if (real_seq_len > 0) {
+     return 128; 
+   } else {
+     ASSERT_CHECK(false);
+   }
+}
+
 void fmhalib_fwd(const void *qkv_ptr,
                  const void *cu_seqlens_ptr,
+                 const void *host_cu_seqlens_ptr,
                  const int total,
                  const int num_heads,
                  const int head_size,
@@ -204,42 +217,88 @@ void fmhalib_fwd(const void *qkv_ptr,
                  const bool zero_tensors,
 		 SeedIncFuncPtr seed_inc_func,
                  cudaStream_t stream,
-                 void *ctx_ptr,
-                 void *s_ptr) {
+                 void *ctx_ptr, // {total, num_heads, head_size}
+                 void *s_ptr) { // {batch_size, num_heads, seq_len, seq_len}
     FMHALIB_BEGIN_FUNC
     auto dprops = GetCurrentDeviceProperties(); 
     ASSERT_CHECK(dprops->major == 8 && dprops->minor == 0);
-    Launch_params<Fused_multihead_attention_fprop_params> launch_params(dprops, stream, is_training, is_nl);
-
-    int seq_len = 512;
-    auto launch = &run_fmha_fp16_512_64_sm80;
-    if( max_seq_len <= 128 ) {
-        seq_len = 128;
-        launch = &run_fmha_fp16_128_64_sm80;
-    } else if( max_seq_len <= 256 ) {
-        seq_len = 256;
-        launch = &run_fmha_fp16_256_64_sm80;
-    } else if( max_seq_len <= 384 ) {
-        seq_len = 384;
-        launch = &run_fmha_fp16_384_64_sm80;
-    } else if( max_seq_len <= 512 ) {
-        seq_len = 512;
-        launch = &run_fmha_fp16_512_64_sm80;
-    } else {
-        ASSERT_CHECK(false);
+    
+    /*
+     *[3, 3, 2, 1]
+     *[0, 3, 6, 8, 9]
+     *seq_len_group_idx: [0, 6, 8, 9]
+     * 
+     * */
+    const int group_size = 3;
+    std::vector<int> seq_len_per_sample(batch_size); 
+    std::vector<int> seq_len_group_idx(group_size);
+    std::vector<int> group_len(group_size);
+    
+    for (int i = 0; i < group_size; i++) {
+      seq_len_group_idx[i] = static_cast<const int*>(host_cu_seqlens_ptr)[batch_size];
+      group_len[i] = 0; 
     }
 
+    int cur_group = 0;
+    int cur_group_len = 1;
+    int cur_idx = 0;
+    seq_len_group_idx[cur_idx++] = 0; 
+    for (int i = 0; i < batch_size; i++) {
+      seq_len_per_sample[i] = static_cast<const int*>(host_cu_seqlens_ptr)[i + 1] - static_cast<const int*>(host_cu_seqlens_ptr)[i];
+      // round so as the elements in array is among [512, 384, 128].
+      seq_len_per_sample[i] = seq_len_round(seq_len_per_sample[i]);
+      if (i > 0) {
+        if (seq_len_per_sample[i] != seq_len_per_sample[i - 1]) {
+	  seq_len_group_idx[cur_idx++] = static_cast<const int*>(host_cu_seqlens_ptr)[i];
+          group_len[cur_group++] = cur_group_len;
+          cur_group_len = 1;	
+	} else {
+          cur_group_len += 1; 
+	} 
+      }
+    }
+    seq_len_group_idx[cur_idx] = static_cast<const int*>(host_cu_seqlens_ptr)[batch_size];
+    group_len[cur_group] = cur_group_len;
+
+#if 1 
+    printf("batch_size = %d\n", batch_size); 
+    printf("group_size = %d, %d, %d\n", group_len[0], group_len[1], group_len[2]);
+#endif
+    if (group_len[0] + group_len[1] + group_len[2] != batch_size) {
+        ASSERT_CHECK(false);
+    }
+#if 1 
+    //for (int i = 0; i < batch_size; i++) {
+    //  printf("seq_len_per_sample[%d] = %d\n", i, seq_len_per_sample[i]);
+    //}
+    for (int i = 0; i < group_size; i++) {
+      printf("seq_len_group_idx[%d] = %d\n", i, seq_len_group_idx[i]);
+    }
+#endif
+
+    Launch_params<Fused_multihead_attention_fprop_params> launch_params_128(dprops, stream, is_training, is_nl);
+    Launch_params<Fused_multihead_attention_fprop_params> launch_params_384(dprops, stream, is_training, is_nl);
+    Launch_params<Fused_multihead_attention_fprop_params> launch_params_512(dprops, stream, is_training, is_nl);
+    
+    int seq_len_512 = 512;
+    int seq_len_384 = 384;
+    int seq_len_128 = 128;
+    auto launch_512 = &run_fmha_fp16_512_64_sm80;
+    auto launch_384 = &run_fmha_fp16_384_64_sm80;
+    auto launch_128 = &run_fmha_fp16_128_64_sm80;
+    
     ASSERT_CHECK(batch_size > 0);
     ASSERT_CHECK(head_size == 64);
 
     if( zero_tensors ) {
         SetZero(ctx_ptr, 2, {total, num_heads, head_size}, stream);  
-        SetZero(s_ptr, 2, {batch_size, num_heads, seq_len, seq_len}, stream);
+        // SetZero(s_ptr, 2, {batch_size, num_heads, seq_len, seq_len}, stream);
+        SetZero(s_ptr, 2, {batch_size, num_heads, 512, 512}, stream);
     }
-
-    set_params(launch_params.params,
-               batch_size,
-               seq_len,
+    
+    set_params(launch_params_512.params,
+               group_len[0], // batch_size,
+               seq_len_512,
                num_heads,
                head_size,
                const_cast<void*>(qkv_ptr),
@@ -247,15 +306,73 @@ void fmhalib_fwd(const void *qkv_ptr,
                ctx_ptr,
                s_ptr,
                p_dropout);
-
-    launch(launch_params, /*configure=*/ true);
-
-    if( is_training ) {
-        int64_t counter_offset = launch_params.elts_per_thread;
-        SetPhiloxCudaState(&launch_params.params.philox_args, seed_inc_func, counter_offset); 
+#if 1 
+    if (group_len[1] > 0) {
+      int qkv_offset = seq_len_group_idx[1] * head_size * num_heads * 3;
+      const __half* new_qkv_ptr = static_cast<const __half*>(qkv_ptr) + qkv_offset;
+      const int* new_cu_seqlens_ptr = static_cast<const int*>(cu_seqlens_ptr) + group_len[0];
+      // {total, num_heads, head_size}
+      __half* new_ctx_ptr = static_cast<__half*>(ctx_ptr) + seq_len_group_idx[1] * num_heads * head_size;
+      // batch_size, num_heads, seq_len, seq_len
+      __half* new_s_ptr = static_cast<__half*>(s_ptr) + group_len[0] * num_heads * 512 * 512; 
+      set_params(launch_params_384.params,
+               group_len[1], // batch_size,
+               seq_len_384,
+               num_heads,
+               head_size,
+               const_cast<void*>(static_cast<const void*>(new_qkv_ptr)),
+               const_cast<void*>(static_cast<const void*>(new_cu_seqlens_ptr)),
+               static_cast<void*>(new_ctx_ptr),
+               static_cast<void*>(new_s_ptr),
+               p_dropout);
+    }
+    if (group_len[2] > 0) {
+      int qkv_offset = seq_len_group_idx[2] * head_size * num_heads * 3;
+      const __half* new_qkv_ptr = static_cast<const __half*>(qkv_ptr) + qkv_offset;
+      const int* new_cu_seqlens_ptr = static_cast<const int*>(cu_seqlens_ptr) + group_len[0] + group_len[1]; 
+      __half* new_ctx_ptr = static_cast<__half*>(ctx_ptr) + seq_len_group_idx[2] * num_heads * head_size;
+      __half* new_s_ptr = static_cast<__half*>(s_ptr) + (group_len[0] + group_len[1]) * num_heads * 512 * 512; 
+      set_params(launch_params_128.params,
+               group_len[2], // batch_size,
+               seq_len_128,
+               num_heads,
+               head_size,
+               const_cast<void*>(static_cast<const void*>(new_qkv_ptr)),
+               const_cast<void*>(static_cast<const void*>(new_cu_seqlens_ptr)),
+               static_cast<void*>(new_ctx_ptr),
+               static_cast<void*>(new_s_ptr),
+               p_dropout);
+    }
+#endif
+    launch_512(launch_params_512, /*configure=*/ true);
+    
+    if (group_len[1] > 0) {
+      launch_384(launch_params_384, /*configure=*/ true);
+    }
+    if (group_len[2] > 0) {
+      launch_128(launch_params_128, /*configure=*/ true);
     }
 
-    launch(launch_params, /*configure=*/ false);
+    if ( is_training ) {
+        int64_t counter_offset = launch_params_512.elts_per_thread;
+        SetPhiloxCudaState(&launch_params_512.params.philox_args, seed_inc_func, counter_offset);
+	if (group_len[1] > 0) {
+          int64_t counter_offset = launch_params_384.elts_per_thread;
+          SetPhiloxCudaState(&launch_params_384.params.philox_args, seed_inc_func, counter_offset);
+	}	
+	if (group_len[2] > 0) {
+          int64_t counter_offset = launch_params_128.elts_per_thread;
+          SetPhiloxCudaState(&launch_params_128.params.philox_args, seed_inc_func, counter_offset);
+	}	
+    }
+    
+    launch_512(launch_params_512, /*configure=*/ false);
+    if (group_len[1] > 0) {
+      launch_384(launch_params_384, /*configure=*/ false);
+    }
+    if (group_len[2] > 0) {
+      launch_128(launch_params_128, /*configure=*/ false);
+    }
     FMHALIB_END_FUNC
 }
 
@@ -325,6 +442,7 @@ static void fmhalib_bwd_nl(const void *dout_ptr,
 void fmhalib_bwd(const void *dout_ptr,
                  const void *qkv_ptr,
                  const void *cu_seqlens_ptr,
+                 const void *host_cu_seqlens_ptr,
                  const int total,
                  const int num_heads,
                  const int head_size,
@@ -358,24 +476,54 @@ void fmhalib_bwd(const void *dout_ptr,
     FMHALIB_BEGIN_FUNC
     auto dprops = GetCurrentDeviceProperties();
     ASSERT_CHECK(dprops->major == 8 && dprops->minor == 0);
-    int seq_len = 512;
-    auto launch = &run_fmha_dgrad_fp16_512_64_sm80;
-    if( max_seq_len <= 128 ) {
-        seq_len = 128;
-        launch = &run_fmha_dgrad_fp16_128_64_sm80;
-    } else if( max_seq_len <= 256 ) {
-        seq_len = 256;
-        launch = &run_fmha_dgrad_fp16_256_64_sm80;
-    } else if( max_seq_len <= 384 ) {
-        seq_len = 384;
-        launch = &run_fmha_dgrad_fp16_384_64_sm80;
-    } else if( max_seq_len <= 512 ) {
-        seq_len = 512;
-        launch = &run_fmha_dgrad_fp16_512_64_sm80;
-    } else {
+    
+    const int group_size = 3;
+    std::vector<int> seq_len_per_sample(batch_size); 
+    std::vector<int> seq_len_group_idx(group_size);
+    std::vector<int> group_len(group_size);
+    
+    for (int i = 0; i < group_size; i++) {
+      seq_len_group_idx[i] = static_cast<const int*>(host_cu_seqlens_ptr)[batch_size];
+      group_len[i] = 0; 
+    }
+
+    int cur_group = 0;
+    int cur_group_len = 1;
+    int cur_idx = 0;
+    seq_len_group_idx[cur_idx++] = 0; 
+    for (int i = 0; i < batch_size; i++) {
+      seq_len_per_sample[i] = static_cast<const int*>(host_cu_seqlens_ptr)[i + 1] - static_cast<const int*>(host_cu_seqlens_ptr)[i];
+      // round so as the elements in array is among [512, 384, 128].
+      seq_len_per_sample[i] = seq_len_round(seq_len_per_sample[i]);
+      if (i > 0) {
+        if (seq_len_per_sample[i] != seq_len_per_sample[i - 1]) {
+	  seq_len_group_idx[cur_idx++] = static_cast<const int*>(host_cu_seqlens_ptr)[i];
+          group_len[cur_group++] = cur_group_len;
+          cur_group_len = 1;	
+	} else {
+          cur_group_len += 1; 
+	} 
+      }
+    }
+    seq_len_group_idx[cur_idx] = static_cast<const int*>(host_cu_seqlens_ptr)[batch_size];
+    group_len[cur_group] = cur_group_len;
+    
+    printf("limin: begin backward\n");
+#if 1
+    printf("batch_size = %d\n", batch_size); 
+    printf("group_size = %d, %d, %d\n", group_len[0], group_len[1], group_len[2]);
+#endif
+    if (group_len[0] + group_len[1] + group_len[2] != batch_size) {
         ASSERT_CHECK(false);
     }
 
+    int seq_len_512 = 512;
+    int seq_len_384 = 384;
+    int seq_len_128 = 128;
+    auto launch_512 = &run_fmha_dgrad_fp16_512_64_sm80;
+    auto launch_384 = &run_fmha_dgrad_fp16_384_64_sm80;
+    auto launch_128 = &run_fmha_dgrad_fp16_128_64_sm80;
+    
     ASSERT_CHECK(batch_size > 0);
     ASSERT_CHECK(head_size == 64);
 
@@ -384,10 +532,12 @@ void fmhalib_bwd(const void *dout_ptr,
     }
 
     Fused_multihead_attention_fprop_params params;
+    Fused_multihead_attention_fprop_params params_384;
+    Fused_multihead_attention_fprop_params params_128;
 
     set_params(params,
-               batch_size,
-               seq_len,
+               group_len[0], //batch_size,
+               seq_len_512, // seq_len,
                num_heads,
                head_size,
                const_cast<void*>(qkv_ptr),
@@ -395,15 +545,77 @@ void fmhalib_bwd(const void *dout_ptr,
                const_cast<void*>(dout_ptr),     // we set o_ptr to dout
                softmax_ptr,  // softmax gets overwritten by dP!
                p_dropout);
+#if 1 
+    int qkv_offset = seq_len_group_idx[1] * head_size * num_heads * 3;
+    const __half* new_qkv_ptr = static_cast<const __half*>(qkv_ptr) + qkv_offset;
+    const int* new_cu_seqlens_ptr = static_cast<const int*>(cu_seqlens_ptr) + group_len[0];
+    const __half* new_dout_ptr = static_cast<const __half*>(dout_ptr) + qkv_offset;
+    // limin-todo: 
+    __half* new_softmax_ptr = static_cast<__half*>(softmax_ptr) + group_len[0] * num_heads * 512 * 512; 
+    if (group_len[1] > 0) {
+      set_params(params_384,
+               group_len[1], // batch_size,
+               seq_len_384, // seq_len,
+               num_heads,
+               head_size,
+               const_cast<void*>(static_cast<const void*>(new_qkv_ptr)),
+               const_cast<void*>(static_cast<const void*>(new_cu_seqlens_ptr)),
+               const_cast<void*>(static_cast<const void*>(new_dout_ptr)),     // we set o_ptr to dout
+               static_cast<void*>(new_softmax_ptr),  // softmax gets overwritten by dP!
+               p_dropout);
+    }
+    int qkv_offset_2 = seq_len_group_idx[2] * head_size * num_heads * 3;
+    const __half* new_qkv_ptr_2 = static_cast<const __half*>(qkv_ptr) + qkv_offset_2;
+    const int* new_cu_seqlens_ptr_2 = static_cast<const int*>(cu_seqlens_ptr) + group_len[0] + group_len[1];
+    const __half* new_dout_ptr_2 = static_cast<const __half*>(dout_ptr) + qkv_offset_2;
+    // {batch_size, num_heads, seq_len, seq_len}
+    __half* new_softmax_ptr_2 = static_cast<__half*>(softmax_ptr) + (group_len[0] + group_len[1]) * num_heads * 512 * 512; 
+    if (group_len[2] > 0) {
+      set_params(params_128,
+               group_len[2], // batch_size,
+               seq_len_128, // seq_len,
+               num_heads,
+               head_size,
+               const_cast<void*>(static_cast<const void*>(new_qkv_ptr_2)),
+               const_cast<void*>(static_cast<const void*>(new_cu_seqlens_ptr_2)),
+               const_cast<void*>(static_cast<const void*>(new_dout_ptr_2)),     // we set o_ptr to dout
+	       // limin-todo: 
+               static_cast<void*>(new_softmax_ptr_2),  // softmax gets overwritten by dP!
+               p_dropout);
+    }
+#endif
 
     // we're re-using these scales
     Data_type acc_type = DATA_TYPE_FP32;
+    // limin-todo: 
     set_alpha(params.scale_bmm1, 1.f, acc_type);
     set_alpha(params.scale_softmax, 1.f / sqrtf(head_size), acc_type);
     set_alpha(params.scale_bmm2, 1.f, DATA_TYPE_FP16);
-    params.dqkv_ptr = dqkv_ptr;
+#if 1 
+    set_alpha(params_384.scale_bmm1, 1.f, acc_type);
+    set_alpha(params_384.scale_softmax, 1.f / sqrtf(head_size), acc_type);
+    set_alpha(params_384.scale_bmm2, 1.f, DATA_TYPE_FP16);
+    
+    set_alpha(params_128.scale_bmm1, 1.f, acc_type);
+    set_alpha(params_128.scale_softmax, 1.f / sqrtf(head_size), acc_type);
+    set_alpha(params_128.scale_bmm2, 1.f, DATA_TYPE_FP16);
+#endif
 
-    launch(params, stream);
+    // limin-todo: 
+    params.dqkv_ptr = dqkv_ptr;
+#if 1 
+    params_384.dqkv_ptr = static_cast<void*>(static_cast<__half*>(dqkv_ptr) + qkv_offset);
+    params_128.dqkv_ptr = static_cast<void*>(static_cast<__half*>(dqkv_ptr) + qkv_offset_2);
+#endif
+    launch_512(params, stream);
+#if 1 
+    if (group_len[1] > 0) {
+      launch_384(params_384, stream);
+    }
+    if (group_len[2] > 0) {
+      launch_128(params_128, stream);
+    }
+#endif
     FMHALIB_END_FUNC
 }
 
